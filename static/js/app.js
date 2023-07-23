0e7fb634a160a225e21d351cf775af3caf4a931b
joeldemontigny <joeldemontigny@yahoo.ca>
2023-07-23 17:48:31 -0400

    sync to main github

--- static/js/app.js
+++ static/js/app.js
@@ -1,35 +1,35 @@
 // 1.Use the D3 library to read in samples.json from the URL 
-// Creating variable for our URL
+// Creating variable for URL
 const url = "https://2u-data-curriculum-team.s3.amazonaws.com/dataviz-classroom/v1.1/14-Interactive-Web-Visualizations/02-Homework/samples.json";
 
-// Promise Pending, this is the middle step for having our data ready when we run the code
-const dataPromise = d3.json(url);
-console.log("Data Promise: ", dataPromise);
+// Get data ready for when we run the code
+const dataReady = d3.json(url);
+console.log("Get Data: ", dataReady);
 
 // Fetch the JSON data and console log it
 d3.json(url).then(function(data) {
     console.log(data);
   });
 
-// 2. Create a horizontal bar chart with a dropdownMenu menu to display the top 10 OTUs found in that individual
-// Load the data using d3.json (our 'data' object contains the properties from our url)
+// Create a horizontal bar chart with a dropdownMenu menu to display the top 10 OTUs found in that individual
+
 d3.json(url).then(function(data) {
   
 // Select the dropdownMenu menu element (the HTML element is a dropdown menu (a <select> element)) and populate it with options
 let dropdownMenu = d3.select("#selDataset");
-// Extract the values for 'names' in the data
+// Extract the values in the data
 let names = data.names;
-// Using 'forEach' method to loop through the 'names' array
+// Use 'forEach' method to loop through the 'names'
 names.forEach(function(name) {
   dropdownMenu.append("option")
           .text(name)
           .property("value", name);
 });
 
-// Set the default ID to display (first value within 'names')
+// Set the default ID (first value within 'names')
 let defaultID = names[0];
 
-// Call the updateBarChart function to create the initial plot
+// Call the updateBarChart
 updateBarChart(defaultID);
 
 // Define the updateBarChart function to update the plot when the dropdownMenu is changed
@@ -51,7 +51,7 @@ function updateBarChart(id) {
   };
 
   
-  // Define the layout for the chart
+  // Chart layout
   let layout = {
     title: "Top 10 OTUs",
     xaxis: { title: "Sample Values" },
@@ -62,8 +62,8 @@ function updateBarChart(id) {
   Plotly.newPlot("bar", [trace1], layout);
 }
 
-// 3. Create a bubble chart that displays each sample
-// Define the updateBubbleChart function to update the plot when the dropdownMenu is changed
+// Create the bubble chart
+
 function updateBubbleChart(id) {
   
     // Filter the data to get selected sample's OTUs
@@ -82,14 +82,14 @@ function updateBubbleChart(id) {
       marker: {
         size: sample_values,
         color: otu_ids,
-        colourscale: "Earth"
+        colourscale: "Jet"
       }
     };
   
     
-    // Define the layout for the chart
+    // Chart layout
     let layout = {
-      title: "Sample Bubble Chart",
+      
       xaxis: { title: "OTU IDs" },
       yaxis: { title: "Sample Values" }
     };
@@ -98,26 +98,26 @@ function updateBubbleChart(id) {
     Plotly.newPlot("bubble", [trace2], layout);
   }
 
-// On change to the DOM, call dropdownMenuChanged()
+// On change to the dropdown, call dropdownMenuChanged()
 d3.select("#selDataset").on("change", dropdownMenuChanged);
 
-// 4.Display the sample metadata, i.e., an individual's demographic information.
-// 5.Display each key-value pair from the metadata JSON object somewhere on the page.
-// Define the updateMetadata function to update the demographic info when the dropdownMenu is changed
+// update output when option changed
+
 function updateMetadata(id) {
 
-    // Filter the data to get the metadata for the selected sample
-    let metadata = data.metadata.filter(function(sample) { return sample.id.toString() === id; })[0];
-  
-    // Select the panel-body element and clear any existing metadata
-    let panelBody = d3.select("#sample-metadata");
-    panelBody.html("");
-  
-    // Use Object.entries to add each key-value pair to the panel-body
-    Object.entries(metadata).forEach(function([key, value]) {
-      panelBody.append("p").text(`${key}: ${value}`);
-    });
-  }
+  // Filter the data to get the metadata for the selected sample
+  let metadata = data.metadata.filter(function(sample) { return sample.id.toString() === id; })[0];
+
+  // Select the new data and clear any existing metadata
+  let newdata = d3.select("#sample-metadata");
+  newdata.html("");
+
+  // Use Object.entries to add each key-value pair to the panel-body
+  Object.entries(metadata).forEach(function([key, value]) {
+    newdata.append("p").text(`${key}: ${value}`);
+  });
+}
+
   
 // Call the updateMetadata function to display the initial demographic info
 updateMetadata(defaultID);
@@ -131,5 +131,5 @@ updateBubbleChart(defaultID);
    updateBubbleChart(id);
    updateMetadata(id);
  }
- 
+
 });
